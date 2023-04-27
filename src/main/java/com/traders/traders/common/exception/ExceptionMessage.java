2eafdb44759d6c48a1b2d1679f47cf8cd0b1fee7
jihwan <sgh002400@naver.com>
2023-04-27 21:57:40 +0900

    feat: 예시 Exception 추가

--- src/main/java/com/traders/traders/common/exception/ExceptionMessage.java
+++ src/main/java/com/traders/traders/common/exception/ExceptionMessage.java
@@ -1,11 +1,14 @@
 package com.traders.traders.common.exception;
 
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
 
 @Getter
 public enum ExceptionMessage {
+	NOT_FOUND_USER_EXCEPTION(NOT_FOUND, "존재하지 않는 유저입니다."),
 	;
 
 	private final HttpStatus httpStatus;
