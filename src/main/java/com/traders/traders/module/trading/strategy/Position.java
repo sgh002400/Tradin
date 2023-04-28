9a437457b1855faf4fdbfdb9e126a66bbfa9ce7f
jihwan <sgh002400@naver.com>
2023-04-28 19:11:10 +0900

    feat: Position 엔티티 생성자 추가

--- src/main/java/com/traders/traders/module/trading/strategy/Position.java
+++ src/main/java/com/traders/traders/module/trading/strategy/Position.java
@@ -8,6 +8,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Embeddable
@@ -21,4 +22,19 @@ public class Position {
 
 	@Column(nullable = false)
 	private String price;
+
+	@Builder
+	private Position(TradingType tradingType, LocalDateTime time, String price) {
+		this.tradingType = tradingType;
+		this.time = time;
+		this.price = price;
+	}
+
+	public static Position of(TradingType tradingType, LocalDateTime time, String price) {
+		return Position.builder()
+			.tradingType(tradingType)
+			.time(time)
+			.price(price)
+			.build();
+	}
 }
