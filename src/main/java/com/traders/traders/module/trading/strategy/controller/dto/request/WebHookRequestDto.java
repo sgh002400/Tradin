e9a1df49c83804fc90ebbd848b6d66ab5f583e20
jihwan <sgh002400@naver.com>
2023-05-09 20:34:55 +0900

    feat: kafka type mapping을 위한 기본 생성자 추가

--- src/main/java/com/traders/traders/module/trading/strategy/controller/dto/request/WebHookRequestDto.java
+++ src/main/java/com/traders/traders/module/trading/strategy/controller/dto/request/WebHookRequestDto.java
@@ -1,16 +1,23 @@
 package com.traders.traders.module.trading.strategy.controller.dto.request;
 
+import javax.validation.constraints.NotBlank;
+
 import com.traders.traders.module.trading.strategy.domain.Position;
 import com.traders.traders.module.trading.strategy.service.dto.WebHookDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class WebHookRequestDto {
-	private final String name;
-	private final Position position;
+	@NotBlank
+	private String name;
+
+	@NotBlank //TODO - test
+	private Position position;
 
 	public WebHookDto toServiceDto() {
 		return WebHookDto.of(name, position);
