92ee06964aaab159c8086f3f3c0ffbbb307e562d
jihwan <sgh002400@naver.com>
2023-05-12 13:30:33 +0900

    fix: JwtUtil, UserService 순환참조 문제 해결

--- src/main/java/com/traders/traders/module/users/service/UsersService.java
+++ src/main/java/com/traders/traders/module/users/service/UsersService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import com.traders.traders.common.exception.TradersException;
 import com.traders.traders.common.jwt.JwtProvider;
 import com.traders.traders.common.jwt.JwtRemover;
-import com.traders.traders.common.jwt.JwtUtil;
 import com.traders.traders.common.utils.PasswordEncoderUtils;
 import com.traders.traders.module.users.controller.dto.response.TokenResponseDto;
 import com.traders.traders.module.users.domain.Users;
@@ -22,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 public class UsersService implements UserDetailsService {
 	private final UsersRepository usersRepository;
 	private final PasswordEncoderUtils passwordEncoderUtils;
-	private final JwtUtil jwtUtil;
 	private final JwtProvider jwtProvider;
 	private final JwtRemover jwtRemover;
 
