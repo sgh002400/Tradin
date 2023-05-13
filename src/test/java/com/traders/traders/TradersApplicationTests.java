454b9991999f5c02079dd84fc99d754ebe8503ef
jihwan <sgh002400@naver.com>
2023-05-14 00:58:56 +0900

    fix: test 임시 삭제

--- src/test/java/com/traders/traders/TradersApplicationTests.java
+++ src/test/java/com/traders/traders/TradersApplicationTests.java
@@ -1,15 +1,5 @@
 package com.traders.traders;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
 class TradersApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
