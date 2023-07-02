ba2919d5e6ec01e72b4a7cee47c678262fc916f5
jihwan <sgh002400@naver.com>
2023-07-02 14:14:37 +0900

    chore: 임시 저장

--- src/main/java/com/traders/traders/module/users/controller/UsersController.java
+++ src/main/java/com/traders/traders/module/users/controller/UsersController.java
@@ -3,7 +3,6 @@ package com.traders.traders.module.users.controller;
 
 import com.traders.traders.module.users.controller.dto.request.ChangeMetadataRequestDto;
 import com.traders.traders.module.users.controller.dto.request.PingRequestDto;
-import com.traders.traders.module.users.controller.dto.request.SignInRequestDto;
 import com.traders.traders.module.users.controller.dto.request.SignUpRequestDto;
 import com.traders.traders.module.users.controller.dto.response.TokenResponseDto;
 import com.traders.traders.module.users.service.UsersService;
@@ -24,15 +23,12 @@ import javax.validation.Valid;
 public class UsersController {
     private final UsersService usersService;
 
-    @PostMapping("/signUp")
+    //클라에서 어디까지 처리해서 줄건지
+    @PostMapping("/auth")
     public ResponseEntity<TokenResponseDto> signUp(@Valid @RequestBody SignUpRequestDto request) {
         return ResponseEntity.ok(usersService.signUp(request.toServiceDto()));
     }
 
-    @PostMapping("/signIn")
-    public ResponseEntity<TokenResponseDto> signIn(@Valid @RequestBody SignInRequestDto request) {
-        return ResponseEntity.ok(usersService.signIn(request.toServiceDto()));
-    }
 
     @Operation(summary = "[인증] 자동매매 탭 - Api&Secret Key 유효성 검사")
     @PostMapping("/binance/ping")
