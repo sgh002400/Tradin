039464071053d27721fde2b201cbabd38777478d
jihwan <sgh002400@naver.com>
2023-07-03 18:58:14 +0900

    hotfix: build 안되는 문제 해결

--- src/test/java/com/tradin/TradinApplicationTests.java
+++ src/test/java/com/tradin/TradinApplicationTests.java
@@ -1,5 +1,13 @@
 package com.tradin;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class TradinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
