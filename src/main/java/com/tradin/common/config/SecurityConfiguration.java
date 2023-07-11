bdb4470d050416c58581f7a96f8927a0cfac9171
jihwan <sgh002400@naver.com>
2023-07-11 17:09:39 +0900

    feat: 스웨거 암호화

--- src/main/java/com/tradin/common/config/SecurityConfiguration.java
+++ src/main/java/com/tradin/common/config/SecurityConfiguration.java
@@ -3,9 +3,13 @@ package com.tradin.common.config;
 import com.tradin.common.filter.JwtAuthenticationFilter;
 import com.tradin.common.filter.JwtExceptionFilter;
 import com.tradin.common.jwt.JwtUtil;
+import com.tradin.common.secret.SecretKeyManager;
+import com.tradin.common.utils.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,18 +21,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+    private final SecretKeyManager secretKeyManager;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests()
-                .mvcMatchers("/swagger-ui/**", "/api-docs/**", "/health-check").permitAll()
+                .mvcMatchers("/health-check").permitAll()
                 .mvcMatchers("/v1/auth/cognito").permitAll()
                 .mvcMatchers("/v1/strategies/future", "/v1/strategies/spot").permitAll()
                 .mvcMatchers("/v1/histories").permitAll()
+                .mvcMatchers("/swagger-ui/**", "/api-docs/**").authenticated()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic().disable()
+                .httpBasic()
+                .and()
                 .formLogin().disable()
                 .logout().disable()
                 .csrf().disable()
@@ -41,5 +49,14 @@ public class SecurityConfiguration {
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class)
                 .build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        final String swaggerUsername = secretKeyManager.getSwaggerUsername();
+        final String swaggerPassword = secretKeyManager.getSwaggerPassword();
+
+        auth.inMemoryAuthentication()
+                .withUser(swaggerUsername).password(passwordEncoder.encode(swaggerPassword)).roles("SWAGGER");
     }
 }
