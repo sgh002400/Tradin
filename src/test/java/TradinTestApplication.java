e6a9c8265b3558580979f18f749603eb9c9459cf
jihwan <sgh002400@naver.com>
2023-07-12 13:47:12 +0900

    feat: 테스트를 위한 dependencies 추가

---
+++ src/test/java/TradinTestApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = TradinTestApplication.class)
+@ActiveProfiles("test")
+public class TradinTestApplication {
+
+}
