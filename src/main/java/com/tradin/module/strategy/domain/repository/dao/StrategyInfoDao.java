30896ffadacffaad7e64ba7fe052066d6bc860ad
jihwan <sgh002400@naver.com>
2023-07-18 20:31:34 +0900

    chore: 스웨거 문서에 Dto 속성 설명 추가

--- src/main/java/com/tradin/module/strategy/domain/repository/dao/StrategyInfoDao.java
+++ src/main/java/com/tradin/module/strategy/domain/repository/dao/StrategyInfoDao.java
@@ -3,6 +3,7 @@ package com.tradin.module.strategy.domain.repository.dao;
 import com.querydsl.core.annotations.QueryProjection;
 import com.tradin.module.strategy.domain.CoinType;
 import com.tradin.module.strategy.domain.TradingType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -11,20 +12,50 @@ import java.time.LocalDateTime;
 public class StrategyInfoDao {
     private final Long id;
     private final String name;
+
+    @Schema(description = "코인명", example = "BITCOIN")
     private final CoinType coinType;
+
+    @Schema(description = "수익팩터")
     private final double profitFactor;
+
+    @Schema(description = "승률")
     private final double winningRate;
+
+    @Schema(description = "단리 기준 수익률")
     private final double simpleProfitRate;
+
+    @Schema(description = "복리 기준 수익률 -> 누적 손익률")
     private final double compoundProfitRate;
+
+    @Schema(description = "총 수익률(수익율+손실율 아님)")
     private final double totalProfitRate;
+
+    @Schema(description = "총 손실률")
     private final double totalLossRate;
+
+    @Schema(description = "평균 수익률")
     private final double averageProfitRate;
+
+    @Schema(description = "총 거래 횟수")
     private final int totalTradeCount;
+
+    @Schema(description = "승리 횟수")
     private final int winCount;
+
+    @Schema(description = "패배 횟수")
     private final int lossCount;
+
+    @Schema(description = "거래 타입", example = "LONG")
     private final TradingType tradingType;
+
+    @Schema(description = "진입 시간")
     private final LocalDateTime time;
+
+    @Schema(description = "진입 가격")
     private final int price;
+
+    @Schema(description = "평균 봉 수")
     private final int averageHoldingPeriod;
 
     @QueryProjection
--- src/main/java/com/tradin/module/strategy/domain/repository/dao/SubscriptionStrategyInfoDao.java
+++ src/main/java/com/tradin/module/strategy/domain/repository/dao/SubscriptionStrategyInfoDao.java
@@ -2,16 +2,27 @@ package com.tradin.module.strategy.domain.repository.dao;
 
 import com.querydsl.core.annotations.QueryProjection;
 import com.tradin.module.strategy.domain.CoinType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class SubscriptionStrategyInfoDao {
     private final long id;
     private final String name;
+
+    @Schema(description = "코인명", example = "BITCOIN")
     private final CoinType coinType;
+
+    @Schema(description = "수익팩터")
     private final double profitFactor;
+
+    @Schema(description = "승률")
     private final double winningRate;
+
+    @Schema(description = "복리 기준 수익률 -> 누적 손익률")
     private final double compoundProfitRate;
+
+    @Schema(description = "평균 수익률")
     private final double averageProfitRate;
 
     @QueryProjection
