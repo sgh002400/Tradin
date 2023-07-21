2fe7727b4f4eba1b00a0c642ddfaa641335fde97
jihwan <sgh002400@naver.com>
2023-07-21 14:44:42 +0900

    refactor: 마지막 매매내역 찾는 로직 수정

--- src/main/java/com/tradin/module/history/domain/repository/HistoryQueryRepository.java
+++ src/main/java/com/tradin/module/history/domain/repository/HistoryQueryRepository.java
@@ -1,9 +1,13 @@
 package com.tradin.module.history.domain.repository;
 
+import com.tradin.module.history.domain.History;
 import com.tradin.module.history.domain.repository.dao.HistoryDao;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HistoryQueryRepository {
+    Optional<History> findLastHistoryByStrategyId(Long id);
+
     List<HistoryDao> findHistoryDaoByStrategyId(Long id);
 }
